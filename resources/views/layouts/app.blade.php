9bcf96685af998d296ebf122caa7a84d2d97d70f
bagusdyuda <bagusdwiwahyuda@gmail.com>
2024-09-12 10:27:55 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png" />
     <link rel="icon" type="image/png" href="../../assets/img/favicon.png" />
     <title>Koperasi - KAMPUS {{ Auth::user()->role }}</title>
-    <!--     Fonts and icons     -->
+    <!-- Fonts and icons -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
@@ -16,47 +16,57 @@
     <!-- Popper -->
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <!-- Main Styling -->
-    <link href="../../assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5" rel="stylesheet" />    
+    <link href="../../assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5" rel="stylesheet" />
+
+    <!-- DataTables CSS (added here) -->
+    <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+
     <!-- Nepcha Analytics (nepcha.com) -->
-    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+    <!-- Nepcha is an easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA, and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
   </head>
 
   <body class="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500">
-    <!-- sidenav  -->
-        @include('layouts.sidebar')
+    <!-- sidenav -->
+    @include('layouts.sidebar')
     <!-- end sidenav -->
+    
     <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
       <!-- Navbar -->
       @include('layouts.header')
-
       <!-- end Navbar -->
 
       <!-- cards -->
       <div class="w-full px-6 py-6 mx-auto">
         <!-- row 1 -->
         @yield('content')
-
-           @include('layouts.footer')
-    </div>
- 
+        @include('layouts.footer')
+      </div>
       <!-- end cards -->
     </main>
-    {{-- <div fixed-plugin>
-      <a fixed-plugin-button class="bottom-7.5 right-7.5 text-xl z-990 shadow-soft-lg rounded-circle fixed cursor-pointer bg-white px-4 py-2 text-slate-700">
-        <i class="py-2 pointer-events-none fa fa-cog"> </i>
-      </a>
-    </div> --}}
 
     @stack('js')
+
+    <!-- plugin for charts  -->
+    <script src="../../assets/js/plugins/chartjs.min.js" async></script>
+    <!-- plugin for scrollbar  -->
+    <script src="../../assets/js/plugins/perfect-scrollbar.min.js" async></script>
+    <!-- GitHub button -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- main script file  -->
+    <script src="../../assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5" async></script>
+
+    <!-- jQuery (required for DataTables) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+    <!-- DataTables JS (added here) -->
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+    <!-- Initialize DataTable (inside script tag) -->
+    <script type="text/javascript">
+      $(document).ready(function() {
+          $('#datatable-basic').DataTable(); // ID of the table to apply DataTable
+      });
+    </script>
   </body>
-  <!-- plugin for charts  -->
-  <script src="../../assets/js/plugins/chartjs.min.js" async></script>
-  <!-- plugin for scrollbar  -->
-  <script src="../../assets/js/plugins/perfect-scrollbar.min.js" async></script>
-  <!-- github button -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- main script file  -->
-  <script src="../../assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5" async></script>
-  
 </html>
