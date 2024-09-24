8507308764b1cf26ddbf49633a1c062eaddabcb2
EkoKun29 <ekoseptyonugroho9@.com>
2024-09-24 10:17:14 +0700

    fetch:logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
   <div class="h-19.5">
     <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
     <a style="text-decoration: none;" class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="{{ route('home') }}">
-      <img src="../../assets/img/logo-ct.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
+      <img src="../../assets/img/logo-kp.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
       <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">KOPERASI KAMPUS {{ Auth::user()->role }}</span>
     </a>
   </div>
