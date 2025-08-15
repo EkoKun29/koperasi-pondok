da913f1a9df1fbe500c7a5edf0cb22427708e5e8
EkoKun29 <ekoseptyonugroho9@gmail.com>
2025-08-15 08:39:58 +0700

    fetch(menambah jarak aksi)

--- resources/views/penjualan/piutang/index.blade.php
+++ resources/views/penjualan/piutang/index.blade.php
@@ -52,12 +52,12 @@
                     <td class="border px-4 py-2">
                         <div class="d-flex">
                               <a href="{{ route('penjualan-piutang.detail', $piu['uuid']) }}"
-                                class="btn btn-info btn-sm">Detail</a>
-                              <a href="javascript:void(0);" data-id="{{ $piu['uuid'] }}" class="btn btn-primary btn-sm editButton">Edit</a>
+                                class="btn btn-info btn-sm ml-2">Detail</a>
+                              <a href="javascript:void(0);" data-id="{{ $piu['uuid'] }}" class="btn btn-primary btn-sm editButton ml-2">Edit</a>
                               <a href="{{ route('delete-penjualan-piutang', $piu['uuid']) }}" id="btn-delete-post" onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data {{ $piu->no_nota }} Ini ??')"
-                                value="Delete" class="btn btn-danger btn-sm">Hapus</a>
+                                value="Delete" class="btn btn-danger btn-sm ml-2">Hapus</a>
                               <a href="{{ route('penjualan-piutang.print', $piu['uuid']) }}"
-                                class="btn btn-secondary btn-sm">Print</a>
+                                class="btn btn-secondary btn-sm ml-2">Print</a>
                         </div>
                     </td>
                 </tr>
